19058dc1fe21fbec61b630bf9595613b5f290bd0
syedammar <ammar_sohail13@hotmail.com>
2021-12-23 11:44:39 +0500

    Create project

---
+++ app/src/main/java/com/example/meditationapp/ui/theme/Theme.kt
@@ -0,0 +1,16 @@
+package com.example.meditationapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MeditationUIYouTubeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
